892585c90f890065d8a861b8ed4d66157b8737f0
Andrea Castegnaro <andrea.castegnaro@outlook.com>
2015-01-12 23:44:59 +0000

    Modified Equation.h, Spirograph.h: added possibility to choose more than one different function, refactoring main

--- Equation.h
+++ Equation.h
@@ -3,25 +3,89 @@
 
 class Equation
 {
-typedef void(EquatFunc)(float*, float*, float);
+    public:
+        struct InputParameters
+        {
+            float params[10];
+            unsigned size;
+
+            InputParameters(){}
+
+            InputParameters(float par[], int s)
+            {
+                for (unsigned i = 0; i < s; i++)
+                {
+                    params[i] = par[i];
+                }                
+                size = s;
+            }
+
+        };
+
+        struct ReturnValues
+        {
+            float returns[10];
+            unsigned size;
+
+            ReturnValues(){}
+
+            ReturnValues(float ret[], int s)
+            {
+                for (unsigned i = 0; i < s; i++)
+                {
+                    returns[i] = ret[i];
+                }
+                size = s;
+            }
+
+        };
+
+
+    typedef void(EquatFunc)(InputParameters*, ReturnValues*, float);
+
+private:
+
+    EquatFunc* equationFunc;
+    InputParameters params;
+    ReturnValues    retValues;
+
 public:
-    //returns the number of arguments taken by the function
-    int GetNumArgs();
-    //returns the number of values returned by the equation, normally either 2 or 3
-    int GetNumReturn();
+
+    //Get/Set the arguments taken by the function
+    InputParameters GetParameters()
+    {
+        return params;
+    }
+
+    void SetParameters(InputParameters par)
+    {
+        params = par;
+    }
+
+    //Set the actual functions that will be called
+    void SetFunc(EquatFunc* func)
+    {
+        equationFunc = func;
+    }
+
+    //Get/Set the values returned by the equation, normally either 2 or 3
+    ReturnValues GetRet()
+    {
+        return retValues;
+    }
 
     //using the equation function, run it with these arguments and return values to this location
     //number of args and return values can be obtained with GetNumArgs, GetNumReturn.
     //T is a unique argument
     //0<T<infinity
-    void Compute(float* args, float* ret, float t);
+    void Compute(float t)
+    {
+        equationFunc(&params,&retValues,t);
+        //Calling the actual function pointer and returning the values
+    }
 
-    void SetFunc(EquatFunc*);
+    
 
-private:
-    //vars
-
-    EquatFunc* equationFunc;
 };
 
 #endif
--- Spirographs.h
+++ Spirographs.h
@@ -9,89 +9,144 @@
 #include "Curve.h"
 
 namespace octet {
-  
-
-
 
 #ifndef PI
-    #define PI 3.1415926535
+#define PI 3.1415926535
 #endif
 #ifndef PI_2
-#define PI_2 6.28318530718
+#define PI_2 2 * PI //6.28318530718
 #endif
-  /// Scene containing a box with octet.
-  class Spirographs : public app {
-    // scene for drawing box
-    ref<visual_scene> app_scene;
-    
-    ref<material> mat;
-    ref<mesh_box> boxMesh;
-  public:
-    /// this is called when we construct the class before everything is initialised.
-    Spirographs(int argc, char **argv) : app(argc, argv) {
+
+    void Compute_2DSpiroGraph(Equation::InputParameters* input, Equation::ReturnValues* retValues, float t)
+    {
+        retValues->size = 2;
+
+        //x = (a - b)*cos(angleIncre*i) + c*cos(((a - b) / b)*(angleIncre*i));
+        retValues->returns[0] = (input->params[0] - input->params[1])*cos(PI_2 * t) + input->params[2] * cos(((input->params[0] - input->params[1]) / input->params[1])*(PI_2 * t));
+        //y = (a - b)*sin(angleIncre*i) - d*sin(((a - b) / b)*(angleIncre*i));
+        retValues->returns[1] = (input->params[0] - input->params[1])*sin(PI_2 * t) + input->params[2] * sin(((input->params[0] - input->params[1]) / input->params[1])*(PI_2 * t));
+
     }
 
-    /// this is called once OpenGL is initialized
-    void app_init() {
-      app_scene =  new visual_scene();
-      app_scene->create_default_camera_and_lights();
 
-      mat = new material(vec4(1, 0, 0, 1));
-      boxMesh = new mesh_box(vec3(0.05));
 
-      float R = 20;
-      float r = 1;
-      float d = 4;
-      float t = 0.5;
+    /// Scene containing a box with octet.
+    class Spirographs : public app {
 
-      int iterations = 2000;
-      int loopMax = iterations *t;
-      float angleIncre = PI_2 / (float)iterations;
-      for (int i = 0; i < loopMax; ++i)
-      {
-          float x = 0, y = 0;
-          
-          x = (R - r)*cos(angleIncre*i) + d*cos(((R - r) / r)*(angleIncre*i));
-          y = (R - r)*sin(angleIncre*i) - d*sin(((R - r) / r)*(angleIncre*i));
-          scene_node* p = new scene_node();
-          app_scene->add_child(p);
-          p->access_nodeToParent()[3] = vec4(x, y, 10, 1);
-          app_scene->add_mesh_instance(new mesh_instance(p, boxMesh, mat));
-      
-      }
-    }
+    private:
+        // scene for drawing box
+        ref<visual_scene> app_scene;
 
-    /// this is called to draw the world
-    void draw_world(int x, int y, int w, int h) {
-        if (is_key_down('W'))
+        ref<material> mat;
+        ref<mesh_box> boxMesh;
+
+        Equation* eq;
+
+        float t = 0.0001f;
+        float t_increase = 0.001f;
+
+        void KeyboardInputControl()
         {
-            app_scene->get_camera_instance(0)->get_node()->access_nodeToParent().translate(0, 1, 0);
+            if (is_key_down('W'))
+            {
+                app_scene->get_camera_instance(0)->get_node()->access_nodeToParent().translate(0, 1, 0);
+            }
+            if (is_key_down('S'))
+            {
+                app_scene->get_camera_instance(0)->get_node()->access_nodeToParent().translate(0, -1, 0);
+            }
+            if (is_key_down('E'))
+            {
+                app_scene->get_camera_instance(0)->get_node()->access_nodeToParent().translate(0, 0, -1);
+            }
+            if (is_key_down('Q'))
+            {
+                app_scene->get_camera_instance(0)->get_node()->access_nodeToParent().translate(0, 0, 1);
+            }
+            if (is_key_down(key::key_up))
+            {
+                GenerateNewStep();
+            }
         }
-        if (is_key_down('S'))
+
+        void GenerateNewStep()
         {
-            app_scene->get_camera_instance(0)->get_node()->access_nodeToParent().translate(0, -1, 0);
+            if (t <= 1.0f)
+            {
+                eq->Compute(t);
+                scene_node* p = new scene_node();
+                app_scene->add_child(p);
+                
+                Equation::ReturnValues values = eq->GetRet();
+                float x = values.returns[0];
+                float y = values.returns[1];
+                p->access_nodeToParent()[3] = vec4(x, y, 10, 1);
+                app_scene->add_mesh_instance(new mesh_instance(p, boxMesh, mat));
+                t += t_increase;
+            }
         }
-        if (is_key_down('E'))
-        {
-            app_scene->get_camera_instance(0)->get_node()->access_nodeToParent().translate(0, 0, -1);
+
+    public:
+        /// this is called when we construct the class before everything is initialised.
+        Spirographs(int argc, char **argv) : app(argc, argv) {
         }
-        if (is_key_down('Q'))
-        {
-            app_scene->get_camera_instance(0)->get_node()->access_nodeToParent().translate(0, 0, 1);
+
+        /// this is called once OpenGL is initialized
+        void app_init() {
+            app_scene = new visual_scene();
+            app_scene->create_default_camera_and_lights();
+
+            mat = new material(vec4(1, 0, 0, 1));
+            boxMesh = new mesh_box(vec3(0.05));
+
+            eq = new Equation();
+            float params[10] = { 20, 1, 4 };
+            eq->SetParameters(Equation::InputParameters(params, 3));
+            eq->SetFunc(Compute_2DSpiroGraph);
+
+            float R = 20;
+            float r = 1;
+            float d = 4;
+            
+            /*int iterations = 2000;
+            int loopMax = iterations * t;
+            float angleIncre = PI_2 / (float)iterations;
+
+            for (float t = 0; t <= 1.0f; t = t + t_increase)
+            {
+                eq->Compute(t);
+            }*/
+            
+            /*for (int i = 0; i < loopMax; ++i)
+            {
+            float x = 0, y = 0;
+
+            x = (R - r)*cos(angleIncre*i) + d*cos(((R - r) / r)*(angleIncre*i));
+            y = (R - r)*sin(angleIncre*i) - d*sin(((R - r) / r)*(angleIncre*i));
+            scene_node* p = new scene_node();
+            app_scene->add_child(p);
+            p->access_nodeToParent()[3] = vec4(x, y, 10, 1);
+            app_scene->add_mesh_instance(new mesh_instance(p, boxMesh, mat));
+
+            }*/
         }
-        
 
-      int vx = 0, vy = 0;
-      get_viewport_size(vx, vy);
-      app_scene->begin_render(vx, vy);
+        /// this is called to draw the world
+        void draw_world(int x, int y, int w, int h) {
 
-      // update matrices. assume 30 fps.
-      app_scene->update(1.0f/30);
+            KeyboardInputControl();
 
-      // draw the scene
-      app_scene->render((float)vx / vy);
+            int vx = 0, vy = 0;
+            get_viewport_size(vx, vy);
+            app_scene->begin_render(vx, vy);
 
-      // tumble the box  (there is only one mesh instance)
-    }
-  };
+            // update matrices. assume 30 fps.
+            app_scene->update(1.0f / 30);
+
+            // draw the scene
+            app_scene->render((float)vx / vy);
+
+            // tumble the box  (there is only one mesh instance)
+        }
+    };
 }
